1d01eabc1c09beb466c2a619e8d97ef2f30383c5
amiraElmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2021-08-06 04:50:51 +0200

    Fixed hangman game's logic
    Added helper fun to solve the sync problem and refactor some code parts

--- js/plugins.js
+++ js/plugins.js
@@ -22,20 +22,20 @@ let word = '', // convert word that player one guessed to array
     ],
     ImageIndex = 1,
     lifeTimes = 6,
-    fillLetterDivs = (word) => {
-        wordCopy = word;
-        word.forEach(element => {
+    fillLetterDivs = (str) => {
+        wordCopy = str;
+        str.forEach(element => {
             addElement('div', `<span class='d-none'>${element}</span>`, lettersDisplay, 'className', 'back-btn p-3 text-uppercase smallest-font animate__heartBeat');
         });
-        wordLength = word.length;
+        wordLength = str.length;
     };
 onePlayerListner.addEventListener('click', (e) => {
     firstPageDiv.classList.add('d-none');
     thirdPageDiv.classList.remove('d-none');
     backBtn.classList.remove('d-none');
-    let randomstring = Math.random().toString(36).substring(2, 8);
-    console.log(randomstring)
-    word = Array.from(randomstring);
+    let randomString = Math.random().toString(36).substring(2, 8);
+    console.log(randomString)
+    word = Array.from(randomString);
     fillLetterDivs(word);
 });
 twoPlayersListner.addEventListener('click', (e) => {
@@ -64,7 +64,7 @@ backBtnListner.addEventListener('click', (e) => {
         wordPart.classList.remove('d-none');
     }
 })
-addElement = function (elementType, elementInnerHTML, parent, property, propertyValue) {
+var addElement = function (elementType, elementInnerHTML, parent, property, propertyValue) {
     element = document.createElement(elementType);
     element.innerHTML = elementInnerHTML;
     parent.appendChild(element);
@@ -82,28 +82,27 @@ document.getElementById('wordForm').addEventListener('submit', (e) => {
     }
 })
 
-document.getElementById('charForm').addEventListener('submit', (e) => {
+var helperFun = async function (index) {
+    await setTimeout(() => {
+        lettersDisplay.removeChild(lettersDisplay.children[index]);
+    },
+        1500
+    );
+}
+
+document.getElementById('charForm').addEventListener('submit', async e => {
     e.preventDefault();
-    allDivs = document.querySelectorAll('.text-uppercase');
-    console.log(word)
+    var allDivs = document.querySelectorAll('.text-uppercase');
     if (word != '' || char.length != 0) {
         char = e.target.char.value;
-        for (let index = 0; index < word.length; index++) {
+        for (let index = 0; index < word.length;) {
             const element = word[index]
             if (element == char) {// if the character is included by word
                 word.splice(index, 1); // that will decrease word length
-                // let divIndex = wordCopy.indexOf(element);
-                let div = allDivs[index];
-                div.children[0].classList.remove('d-none');
-                // allDivs[index].children[0].classList.add('animate__heartBeat');
-                // allDivs[index].classList.add('wow');
-                // allDivs[index].classList.add('animate__heartBeat');
-                setTimeout(() => {
-                    lettersDisplay.removeChild(div);
-                    index--; // as the array length will decrease by one
-                },
-                    2000
-                );
+                await helperFun(index)
+                await allDivs[index].children[0].classList.remove('d-none');
+            } else {
+                index++;
             }
         }
         if (word.length == wordLength) { // the gussed char. isn't correct
@@ -122,7 +121,13 @@ document.getElementById('charForm').addEventListener('submit', (e) => {
             // update wordLength var. for next comparison
             messageDisplay.textContent = 'good guessing';
             wordLength = word.length;
-            if (wordLength != 0) messageDisplay.textContent = `just ${wordLength} guess to win`;
+            if (wordLength != 0) {
+                setTimeout(() => {
+                    messageDisplay.textContent = `just ${wordLength} more guesses to win`;
+                },
+                    1000
+                );
+            }
             else {
                 messageDisplay.innerHTML = '<span>Congratulations ^_^, </span><span>Go back to play again</span>';
                 document.getElementById('charForm').classList.add('d-none');
@@ -135,16 +140,16 @@ document.getElementById('charForm').addEventListener('submit', (e) => {
     document.getElementById('charForm').reset()
 })
 
-document.getElementById('showBtn').addEventListener('click', (e) => {
-    allDivs = document.querySelectorAll('.text-uppercase');
-    allDivs.forEach(div => {
-        div.children[0].classList.remove('d-none');
-    })
-    setTimeout(() =>
-        allDivs.forEach(div => {
-            div.children[0].classList.add('d-none');
-            document.getElementById('showBtn').classList.add('d-none');
-        }),
-        300
-    );
-})
+// document.getElementById('showBtn').addEventListener('click', (e) => {
+//     var allDivs = document.querySelectorAll('.text-uppercase');
+//     allDivs.forEach(div => {
+//         div.children[0].classList.remove('d-none');
+//     })
+//     setTimeout(() =>
+//         allDivs.forEach(div => {
+//             div.children[0].classList.add('d-none');
+//             document.getElementById('showBtn').classList.add('d-none');
+//         }),
+//         300
+//     );
+// })
